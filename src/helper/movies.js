240e50b416f469342109158401c73a97ec9315b5
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-06-10 23:48:50 -0700

    Add files via upload

---
+++ src/helper/movies.js
@@ -0,0 +1,47 @@
+
+
+
+export var movies = {
+  0: {
+    title: 'The Journey to West Palm Dale',
+    year: 2003,
+    description: "Friends get together for an epic road trip.",
+    genre: 'comedy'
+  },
+  1: {
+    title: 'The Cool Cat',
+    year: 2010,
+    description: "a man gets a cat for the first time and the adventures that ensue.",
+    genre: 'comedy'
+  },
+  2: {
+    title: 'The Zebra Man',
+    year: 2013,
+    description: "always an outcast, this story is about the origins of the latest superhero.",
+    genre: 'drama'
+  },
+  3: {
+    title: 'The Spaceship',
+    year: 2017,
+    description: "NASA expedition #838",
+    genre: 'drama'
+  },
+  4: {
+    title: 'The First Kiss',
+    year: 2012,
+    description: "They meet in the most unlikeliest of circumstances, will they get together, of course!",
+    genre: 'romance'
+  },
+  5: {
+    title: 'A way over there',
+    year: 2015,
+    description: "A classic story of growing up and finding your path",
+    genre: 'classic'
+  },
+  6: {
+    title: 'The Pink Mercedes',
+    year: 2000,
+    description: "a hustler somehow stumbles upon this new car.",
+    genre: 'comedy'
+  }
+}
